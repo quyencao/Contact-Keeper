b35d754caef7865317ca19f88874dc1708cbe05a
quyen9x1995 <boy20195@gmail.com>
2019-06-23 17:49:09 +0700

    Use body-parser middleware and validate with express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+
 const router = express.Router();
 
 // @route   POST /api/users
 // @desc    Register User
 // @access  Public
-router.post("/", (req, res, next) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please enter name")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json());
+
 app.get("/", (req, res, next) => {
   res.json({ msg: "Welcome to Contact Keeper Api..." });
 });
