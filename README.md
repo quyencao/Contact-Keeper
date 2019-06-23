c87c69dc1e65330a3241a147c98fe68d2a60dc4e
quyen9x1995 <boy20195@gmail.com>
2019-06-23 17:23:24 +0700

    Connect to MongoDB

---
+++ README.md
@@ -0,0 +1,1 @@
+## Change file config/default.sample.json to config/default.json and provide mongoDB URI
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.get("/", (req, res, next) => {
   res.json({ msg: "Welcome to Contact Keeper Api..." });
